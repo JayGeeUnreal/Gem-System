d9aa7baddc40db809772bd0893f6b39ed84de72b
jorgen41 <jorgen41@gmail.com>
2025-11-09 06:55:53 +0100

    Added reletive paths

--- start_scripts/start_control_panel.bat
+++ start_scripts/start_control_panel.bat
@@ -1,6 +1,9 @@
+@echo off
 TITLE ControlPanel
-cd C:\Users\jorge\Documents\AI\Gem-System
-call C:\Users\jorge\miniconda30\Scripts\activate.bat
+cd /d "%~dp0.."
+:: Edit this line to match your installation path for Anaconda
+call %USERPROFILE%\miniconda30\Scripts\activate.bat
+::
 call conda activate mcp_env_1
 python control_panel.py
 cmd /k
